9974147248f57d0d264ead38a999be8afd008385
coaxial <hi@64b.it>
2018-08-22 22:40:48 -0400

    Fix firewall

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -18,3 +18,6 @@ sudo sed -i 's/^\(root:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subuid
 sudo sed -i 's/^\(lxd:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subgid
 sudo sed -i 's/^\(root:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subgid
 sudo systemctl restart lxd
+
+ansible --version
+molecule --version
