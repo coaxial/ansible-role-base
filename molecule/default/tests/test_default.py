9b03ff906b4fff4394810f293a377cf2addc024d
coaxial <c@64b.it>
2026-02-08 10:09:49 +0100

    <3 <3 <3 linting

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -45,11 +45,14 @@ def test_sshd_config(host):
     assert f.mode == 0o644
     assert 'PermitRootLogin no' in f.content_string
     assert 'PasswordAuthentication no' in f.content_string
-    
+
     # Dynamically check AllowUsers using Ansible variables
-    provisioning_username = host.ansible.get_variables()['base__provisioning_username']
+    provisioning_username = (host.ansible
+                             .get_variables()['base__provisioning_username'])
     operator_username = host.ansible.get_variables()['base__operator_username']
-    expected_allow_users = f'AllowUsers {provisioning_username} {operator_username}'
+    expected_allow_users = (
+            f'AllowUsers {provisioning_username} {operator_username}'
+    )
     assert expected_allow_users in f.content_string
 
 
